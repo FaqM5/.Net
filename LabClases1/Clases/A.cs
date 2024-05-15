ba85b92a8f01f96f6985aa56555b4cae0f5afa72
FaqM5 <140659987+FaqM5@users.noreply.github.com>
2024-05-15 20:57:33 -0300

    Add files via upload

---
+++ LabClases1/Clases/A.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection.Metadata;
+
+    namespace Clases
+    {
+            public class A
+            {
+
+            private string nombreInstancia;
+
+            public string NombreInstancia
+            {
+                get { return nombreInstancia; }
+                set { nombreInstancia = value; }
+            }
+            public A()
+                {
+                    this.nombreInstancia = "Instancia sin nombre";
+                }
+
+            public A(string n)
+            {
+                this.nombreInstancia = n;
+
+            }
+
+            public void MostrarNombre()
+            {
+                System.Console.WriteLine(nombreInstancia);
+
+            }
+
+            public  void M1()
+            {
+                System.Console.WriteLine("El metodo M1 fue invocado");
+            }
+
+            public  void M2()
+            {
+                System.Console.WriteLine("El metodo M2 fue invocado");
+            }
+            public  void M3()
+            {
+                System.Console.WriteLine("El metodo M3 fue invocado");
+            }
+
+        }
+    }
+
---
+++ LabClases1/Clases/B.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class B : A
+    {
+        public  void M4()
+        {
+            System.Console.WriteLine("El metodo del hijo Invocado");
+        }
+
+        public B() : base("Instancia de B")
+        {
+
+        }
+    }
+}
